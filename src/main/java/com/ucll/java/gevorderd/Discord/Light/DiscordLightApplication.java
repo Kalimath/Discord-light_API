8ec3f6eeaebe670c6327a623bcd26d8489a6bdae
Arno <arnobarre15@gmail.com>
2020-11-04 14:44:00 +0100

    first commit

---
+++ src/main/java/com/ucll/java/gevorderd/Discord/Light/DiscordLightApplication.java
@@ -0,0 +1,14 @@
+package com.ucll.java.gevorderd.Discord.Light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordLightApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DiscordLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucll/java/gevorderd/Discord/Light/DiscordLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucll.java.gevorderd.Discord.Light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
