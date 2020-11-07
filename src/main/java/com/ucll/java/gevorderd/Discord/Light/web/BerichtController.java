8e563c72d94252586d52c801669c619a38f68d25
Kalimath <mathieu_broe@yahoo.com>
2020-11-07 17:11:32 +0100

    Endpoint: kanaal aanmaken

--- src/main/java/com/ucll/java/gevorderd/Discord/Light/web/BerichtController.java
+++ src/main/java/com/ucll/java/gevorderd/Discord/Light/web/BerichtController.java
@@ -1,7 +1,11 @@
 package com.ucll.java.gevorderd.Discord.Light.web;
 
 import com.ucll.java.gevorderd.Discord.Light.dao.BerichtDao;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("berichten")
+@RestController
 public class BerichtController {
 
     private final BerichtDao berichtDao;
--- src/main/java/com/ucll/java/gevorderd/Discord/Light/web/KanaalController.java
+++ src/main/java/com/ucll/java/gevorderd/Discord/Light/web/KanaalController.java
@@ -1,7 +1,14 @@
 package com.ucll.java.gevorderd.Discord.Light.web;
 
 import com.ucll.java.gevorderd.Discord.Light.dao.KanaalDao;
+import com.ucll.java.gevorderd.Discord.Light.domain.Kanaal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("kanalen")
 public class KanaalController {
 
     private final KanaalDao kanaalDao;
@@ -10,4 +17,8 @@ public class KanaalController {
         this.kanaalDao = kanaalDao;
     }
 
+    @PostMapping("/")
+    public Kanaal voegKanaalToe(@RequestBody Kanaal kanaal){
+        return kanaalDao.save(kanaal);
+    }
 }
